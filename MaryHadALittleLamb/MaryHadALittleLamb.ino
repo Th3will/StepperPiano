eefa9d2624b1788ac38935e77b8b7af1372ffedb
Th3will <wni1717@gmail.com>
2022-12-01 14:56:49 -0800

    CoNcUrReNcY

--- MaryHadALittleLamb/MaryHadALittleLamb.ino
+++ MaryHadALittleLamb/MaryHadALittleLamb.ino
@@ -9,7 +9,10 @@ int del;
 int use=180;
 int tempo=120;
 int oct=5;
-const int notes[8] = {1912, 1703, 1517, 1431, 1275, 1136, 1012, 956};
+//int notes[8] = {1912, 1703, 1517, 1431, 1275, 1136, 1012, 956};
+const int notes[8] = {956, 851, 758, 715, 637, 568, 506, 478};
+int motDel[3] = {0,0,0};
+//bool motState[3] = {false,false,false};
 
 void setup() {
   // Sets the two pins as Outputs
@@ -33,17 +36,22 @@ void setup() {
   pinMode(6, INPUT_PULLUP);
   pinMode(7, INPUT_PULLUP);
 
+  digitalWrite(dirPin[0],dir);
+  digitalWrite(dirPin[1],dir);
+  digitalWrite(dirPin[2],dir);
+
+
 }
 void loop() {
   int motorId = 0;
-  oct=5;
   for (int i = 0; i < 8; i++)
   {
-    /* code */
     if(digitalRead(i) == LOW && motorId < 3){
-      note(notes[i],35,motorId);
+      note(notes[i],10,motorId);
+      //motDel[motorId] = notes[i];
       motorId++;
     }
+    //play();
   }
   
  
@@ -51,16 +59,25 @@ void loop() {
 }
 
 void note(int num,long dur,int motor) {
-  del=(num*oct)/10;
-  //dir=!dir;
- digitalWrite(dirPin[motor],dir);
-  coun=floor((dur*5*tempo)/del);
-  for(int x = 0; x < coun; x++) {
+  /* coun=floor((dur*5*tempo)/num);
+  for(int x = 0; x < coun; x++) { */
     digitalWrite(stepPin[motor],HIGH);
-    delayMicroseconds(del);
+    delayMicroseconds(num);
     digitalWrite(stepPin[motor],LOW);
-    delayMicroseconds(del);
-  }
-
+    delayMicroseconds(num);
+  //}
 }
+void play(){
+  for (int i = 0; i < 3; i++)
+  {
+    
+  }
+  
+}
+
+   int note(int num){
+    del = (num*oct)/10;
+    return del;
+  } 
+
 
