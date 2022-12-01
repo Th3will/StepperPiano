1d87c3332fd6777c2e8efe7454be352dc8419212
Th3will <wni1717@gmail.com>
2022-11-30 23:08:07 -0800

    guaranteed

--- MaryHadALittleLamb/MaryHadALittleLamb.ino
+++ MaryHadALittleLamb/MaryHadALittleLamb.ino
@@ -1,92 +1,101 @@
-#include <Arduino.h>
-//#include <cmath>
-//#include "A4988.h "
-#include "Motor.h"
-
-
-/*TODO
-  Implement a motor playing solution that works
-  Make Motor Scheduler w/ cases
-  Make everything into a struct and cleanup
-*/
-
-
-
+const int stepPin = 9; 
+const int dirPin = 8; 
 
 // here comes a bunch of 'useful' vars; dont mind
 int coun;
-int oct = 5;int del;
 bool dir=0;
-const int use=180;
-const int tempo=120;
-const long dur = 50;
-
-const int notes[8] = {2024, 1912, 1703, 1517, 1431, 1275, 1136, 1012};
-
-//Motor pins; Left->Mid->Right
-const int motorDir[3] = {8,10,12};
-const int motorStep[3] = {9,11,13};
-//cycles through during loop to schedule which motor
-
-
-//Motor Initialization
-void motorInit(){
-  //motor right
-    pinMode(motorStep[0],OUTPUT); 
-    pinMode(motorDir[0],OUTPUT);
-    //motor mid
-    pinMode(motorStep[1],OUTPUT); 
-    pinMode(motorDir[1],OUTPUT);
-    //motor left
-    pinMode(motorStep[2],OUTPUT); 
-    pinMode(motorDir[2],OUTPUT);
-}
-
-//Key Initialization
-void keyInit(){
-    pinMode(0,INPUT_PULLUP);
-    pinMode(1,INPUT_PULLUP);
-    pinMode(2,INPUT_PULLUP);
-    pinMode(3,INPUT_PULLUP);
-    pinMode(4,INPUT_PULLUP);
-    pinMode(5,INPUT_PULLUP);
-    pinMode(6,INPUT_PULLUP);
-    pinMode(7,INPUT_PULLUP);
-
-}
-
-//play music
-void note(int num, int motorID) {
-  del=(num*oct)/10;
-  digitalWrite(motorDir[motorID],dir);
-  //x in x*5*temp is duration in milliseconds
-  coun=floor((dur*5*tempo)/del);
-  for(int x = 0; x < coun; x++) {
-    digitalWrite(motorStep[motorID],HIGH);
-    delayMicroseconds(del);
-    digitalWrite(motorStep[motorID],LOW);
-    delayMicroseconds(del);
-  }
-  motorID++;
-  Serial.println("detected note nextMotor");
-}
+int del;
+int c=1912;
+int cf=1805;
+int d= 1703;
+int df=1607;
+int e=1517;
+int f=1431;
+int ff=1351;
+int g= 1275;
+int gf=1203;
+int a=1136;
+int af=1072;
+int b=1012;
+int c1=floor(c/2);
+int cf1=floor(cf/2);
+int d1=floor(d/2);
+int df1=floor(df/2);
+int e1=floor(e/2);
+int f1=floor(1431/2);
+int ff1=floor(1351/2);
+int g1= floor(1275/2);
+int gf1=floor(1203/2);
+int a1=floor(1136/2);
+int af1=floor(1072/2);
+int b1=floor(1012/2);
+int e0=e*2;
+int g0=g*2;
+int b0=b*2;
+int af0=af*2;
+int a0=a*2;
+int f0=f*2;
+int use=180;
+int tempo=120;
+int oct=5;
 
 void setup() {
-    motorInit();
-    keyInit();
-    Serial.begin(9600);
-}
+  // Sets the two pins as Outputs
+  Serial.begin(9600);
 
+  pinMode(stepPin,OUTPUT); 
+  pinMode(dirPin,OUTPUT);
+  
+  pinMode(0, INPUT_PULLUP);
+  pinMode(1, INPUT_PULLUP);
+  pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
+  pinMode(5, INPUT_PULLUP);
+  pinMode(6, INPUT_PULLUP);
+  pinMode(7, INPUT_PULLUP);
+
+}
 void loop() {
-  int motorID = 0;
-  for (int i = 0; i < 8; i++)
-  {
-    //check button pressed and hasn't already scheduled all other avai
-    if(digitalRead(i)==LOW&&motorID < 3){
-      note(notes[i],motorID);
-    }
-  }
-  //reset motorID for next loop through;
+  oct=5;
+  int sensorVal0 = digitalRead(0);
+  int sensorVal1 = digitalRead(1);
+  int sensorVal2 = digitalRead(2);
+  int sensorVal3 = digitalRead(3);
+  int sensorVal4 = digitalRead(4);
+  int sensorVal5 = digitalRead(5);
+  int sensorVal6 = digitalRead(6);
+  int sensorVal7 = digitalRead(7);
+
+// Here you put your music code
+
+  if(sensorVal0 == LOW) note(c,35);
+  if(sensorVal1 == LOW) note(d,35);
+  if(sensorVal2 == LOW) note(e,35);
+  if(sensorVal3 == LOW) note(f,35);
+  if(sensorVal4 == LOW) note(g,35);
+  if(sensorVal5 == LOW) note(a,35);
+  if(sensorVal6 == LOW) note(b,35);
+  if(sensorVal7 == LOW) note(c/2,35);
+
 }
 
+void note(int num,long dur) {
+  del=(num*oct)/10;
+  //dir=!dir;
+ digitalWrite(dirPin,dir);
+  coun=floor((dur*5*tempo)/del);
+  for(int x = 0; x < coun; x++) {
+    digitalWrite(stepPin,HIGH);
+    delayMicroseconds(del);
+    digitalWrite(stepPin,LOW);
+    delayMicroseconds(del);
+  }
 
+}
+
+void pa(int durp){
+  int ker=floor(durp/100)*tempo;
+delay(ker);
+  
+  }
